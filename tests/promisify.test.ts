bf538bc686772bd343923c3d0da74fa0aff998c7
Livshitz <elya.livshitz@gmail.com>
2020-05-19 09:54:51 +0300

    + more tests

--- tests/promisify.test.ts
+++ tests/promisify.test.ts
@@ -5,11 +5,40 @@ const newPromise = () => {
     return promise;
 }
 
+const measure = async (fn: ()=>Promise<any>, name='') => {
+	let t0 = performance.now();
+	let res = null;
+	let duration = 0;
+	try {
+		res = await fn();
+	} catch (ex) {
+		return ex;
+	} finally {
+		duration = performance.now() - t0;
+		console.log(`⏱	'${name}' took ${duration.toFixed(2)}ms`);
+	}
+	return duration; //res;
+}
+
 beforeAll(()=> {
 })
 
-test('newPromise-positive', () => {
+test('newPromise-simple', () => {
 	let p = Promisify.new();
 	p.resolve(true);
 	expect(p).resolves.toEqual(true);
+});
+
+test('newPromise-setTimeout', async () => {
+	let p = Promisify.new();
+	let res = null;
+
+	let dur = await measure(async ()=>{
+		setTimeout(()=>{
+			p.resolve(true);
+		}, 100);
+		res = await p;
+	}, 'setTimeout');
+	expect(res).toBe(true);
+	expect(dur).toBeLessThanOrEqual(150);
 });
