1def8b1670f24b545648e8cea1d0755b535482fa
Livshitz <elya.livshitz@gmail.com>
2020-05-23 23:09:33 +0300

    * fix browser version

--- src/browser.ts
+++ src/browser.ts
@@ -1,5 +1,5 @@
 import * as all from './index';
+const concurrency = {...all};
 
-const libx = {...all};
-
-(<any>Window).libx = libx;
+if ((<any>window).libx == undefined) (<any>window).libx = {};
+(<any>window).libx.concurrency = concurrency;
