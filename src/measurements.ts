b9c3f13fc5d3ce78e51985517096d001d0799794
Elya Livshitz <elya.livshitz@gmail.com>
2021-11-26 16:36:11 +0200

    * changing measurements to bit more clear and add `measure` function to execute a fn and return duration

--- src/measurements.ts
+++ src/measurements.ts
@@ -1,19 +1,26 @@
 
 let _measures = {};
 
-export const measure = function(measureName = '_') {
-    if (_measures[measureName] != null) return getMeasure(measureName);
+export const measure = async function<T=any>(fn: ()=>Promise<T>|T) {
+    const start = new Date().getTime();
+    await fn();
+    const duration = new Date().getTime() - start;
+    return duration;
+}
+
+export const startMeasure = function(measureName = '_') {
+    if (_measures[measureName] != null) return peekMeasure(measureName);
     _measures[measureName] = new Date().getTime();
     return 0;
 }
 
-export const getMeasure = function(measureName = '_') {
+export const peekMeasure = function(measureName = '_') {
     if (_measures[measureName] == null) return 0;
     return new Date().getTime() - _measures[measureName];
 }
 
 export const getMeasureAndReset = function(measureName = '_') {
-    let ret = getMeasure(measureName);
+    let ret = peekMeasure(measureName);
     _measures[measureName] = null;
     return ret;
 }
